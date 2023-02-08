103388e15bdba64489338bfa033e6b93650f4d40
sa171 <rudrawars@gmail.com>
2023-02-08 12:59:14 -0700

    filled upload logic

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
     response = {'message':"Welcome to flask controller!"}
     return jsonify(response),200
 
-@app.route('/upload',methos=['POST'])
+@app.route('/upload',methods=['POST'])
 def upload():
     if 'image' not in request.json:
         return jsonify({'message': 'Bad Request'}), 400
@@ -20,17 +20,17 @@ def upload():
         sqs = boto3.client('sqs')
         queue_list = sqs.list_queues(QueueNamePrefix='request_queue')
         if len(queue_list) == 0:
-            queue = sqs.create_queue(QueueName="request_queue")
+            sqs.create_queue(QueueName="request_queue")
         queueUrl = sqs.get_queue_url(QueueName="request_queue")['QueueUrl']
-        image_data = file.read()
         message = {
             'Id':"Id",
-            'image':""
+            'image':base64.b64encode(file.read())
         }
         response = sqs.send_message(
             QueueUrl= queueUrl,
-            MessageBody=image_data
+            MessageBody=message
         )
+        app.logger.info(response)
         app.logger.info("Image sent to SQS successfully.")
 # Do we need to decouple response logic from request logic?
         queue_list = sqs.list_queues(QueueNamePrefix="response_queue")
@@ -49,3 +49,5 @@ def upload():
         app.logger.error(exp)
         return jsonify({'message':"Some issue with the server"}),500
     
+if __name__ == '__main__':
+    app.run(threaded=True,host="127.0.0.1",port=5000)
