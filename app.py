ca2f7948fa1f159180850f89d372d319f682238b
sa171 <rudrawars@gmail.com>
2023-02-14 22:34:21 -0700

    Fixed logic for sending message

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 import logging
 import boto3
 import base64
+import uuid
 
 app = Flask(__name__)
 logging.basicConfig(filename='flask.log', level=logging.DEBUG)
@@ -13,17 +14,18 @@ def index():
 
 @app.route('/upload',methods=['POST'])
 def upload():
-    if 'image' not in request.json:
-        return jsonify({'message': 'Bad Request'}), 400
+    # if 'myfile' not in request.json:
+    #     return jsonify({'message': 'Bad Request'}), 400
     try:
         file = request.files['myfile']
-        sqs = boto3.client('sqs')
-        queue_list = sqs.list_queues(QueueNamePrefix='request_queue')
-        if len(queue_list) == 0:
-            sqs.create_queue(QueueName="request_queue")
-        queueUrl = sqs.get_queue_url(QueueName="request_queue")['QueueUrl']
+        sqs = boto3.resource('sqs')
+        queue = sqs.get_queue_by_name(QueueName='request_queue')
+        # if len(queue_list) == 0:
+        #     sqs.create_queue(QueueName="request_queue")
+        app.logger.info("Received SQS queue url:",queue.url)
+        queueUrl = queue.url
         message = {
-            'Id':"Id",
+            'Id':str(uuid.uuid4()),
             'image':base64.b64encode(file.read())
         }
         response = sqs.send_message(
@@ -32,22 +34,19 @@ def upload():
         )
         app.logger.info(response)
         app.logger.info("Image sent to SQS successfully.")
-# Do we need to decouple response logic from request logic?
-        queue_list = sqs.list_queues(QueueNamePrefix="response_queue")
-        if len(queue_list) == 0:
-            queue = sqs.create_queue(QueueName="response_queue")
-        queueUrl = sqs.get_queue_url(QueueName="response_queue")['QueueUrl']
+        # Receive logic - Check all messages from receive queue and delete the one that matches the ID
+        '''
+        queue = sqs.get_queue_by_name(QueueName="response_queue")
         response = sqs.receive_message(
-            QueueUrl=queueUrl,
-            MaxNumberOfMessages=1,
+            QueueUrl=queue.url,
+            MaxNumberOfMessages=20,
             WaitTimeSeconds=20
         )
-        
-
-
+        '''
+        return jsonify({'message':'Message sent successfully'}),200
     except Exception as exp:
         app.logger.error(exp)
         return jsonify({'message':"Some issue with the server"}),500
     
 if __name__ == '__main__':
-    app.run(threaded=True,host="127.0.0.1",port=5000)
+    app.run(threaded=True,host="127.0.0.1",port=5000,debug=True)
