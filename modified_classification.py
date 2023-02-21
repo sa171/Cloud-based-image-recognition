f5136620c1c166b288c2d74cc427e1a87243ff27
sa171 <rudrawars@gmail.com>
2023-02-21 00:05:15 -0700

    @sa171 quick fix

--- modified_classification.py
+++ modified_classification.py
@@ -29,71 +29,72 @@ queue_url = 'https://sqs.us-east-1.amazonaws.com/874290406143/request_queue'
 # test_request_queue = 'https://sqs.us-east-1.amazonaws.com/874290406143/p1-test2-sqs1'
 response_queue_url = 'https://sqs.us-east-1.amazonaws.com/874290406143/response-queue'
 
-
-while True:
-    response = sqs.receive_message(
-        QueueUrl=queue_url,
-        MaxNumberOfMessages=1,
-        WaitTimeSeconds=20
-    )
-    if 'Messages' in response:
-        message = response['Messages'][0]
-        logging.debug('Received message from SQS', message)
-        message_body = json.loads(message['Body'])
-        image_data = message_body['image']
-        # Logging ID for tracing
-        logging.debug(f"Processing message Id: {message_body['id']}")
-        # Decode the base64-encoded image data
-        decoded_data = base64.b64decode(image_data)
-
-        # Load the decoded data into a PIL image
-        img = Image.open(BytesIO(decoded_data))
-        model = models.resnet18(pretrained=True)
-
-        model.eval()
-        img_tensor = transforms.ToTensor()(img).unsqueeze_(0)
-        outputs = model(img_tensor)
-        _, predicted = torch.max(outputs.data, 1)
-
-        with open('./imagenet-labels.json') as f:
-            labels = json.load(f)
-        result = labels[np.array(predicted)[0]]
-        img_name = "received_image"
-        # save_name = f"({img_name}, {result})"
-        save_name = f"{img_name},{result}"
-        print(f"{save_name}")
-        logging.info("Classification result of {} is {}".format(message_body['id'],result))
-        # storing output in output bucket
-        input_bucket_name = 'output-bucket-results231'
-        s3 = boto3.client('s3', region_name='us-east-1')
-        s3.put_object(
-            Bucket=input_bucket_name,
-            Key=id,
-            Body=result
-        )
-        logging.info("Image sent to Output Bucket successfully.")
-        # Send classified image results to SQS Response Queue
-        message_response = {
-            'id':message_body['id'],
-            'results':result
-        }
-        response = sqs.send_message(
-            QueueUrl= response_queue_url,
-            MessageBody= json.dumps(message_response)
-        )
-
-        # Delete the message from the queue
-        logging.info("Deleting message from response queue")
-        receipt_handle = message['ReceiptHandle']
-        sqs.delete_message(
+try:
+    while True:
+        response = sqs.receive_message(
             QueueUrl=queue_url,
-            ReceiptHandle=receipt_handle
+            MaxNumberOfMessages=1,
+            WaitTimeSeconds=20
         )
-        # Introduce a delay
-        logging.info("Sleeping for 5 sec")
-        time.sleep(5)
-        logging.info("Awake!")
-    else:
-        logging.info("No messages in queue")
-#img = Image.open(urlopen(url))
+        if 'Messages' in response:
+            message = response['Messages'][0]
+            logging.debug('Received message from SQS', message)
+            message_body = json.loads(message['Body'])
+            image_data = message_body['image']
+            # Logging ID for tracing
+            logging.debug(f"Processing message Id: {message_body['id']}")
+            # Decode the base64-encoded image data
+            decoded_data = base64.b64decode(image_data)
+
+            # Load the decoded data into a PIL image
+            img = Image.open(BytesIO(decoded_data))
+            model = models.resnet18(pretrained=True)
+
+            model.eval()
+            img_tensor = transforms.ToTensor()(img).unsqueeze_(0)
+            outputs = model(img_tensor)
+            _, predicted = torch.max(outputs.data, 1)
+
+            with open('./imagenet-labels.json') as f:
+                labels = json.load(f)
+            result = labels[np.array(predicted)[0]]
+            img_name = "received_image"
+            # save_name = f"({img_name}, {result})"
+            # save_name = f"{img_name},{result}"
+            # print(f"{save_name}")
+            logging.info("Classification result of {} is {}".format(message_body['id'],result))
+            # storing output in output bucket
+            output_bucket_name = 'output-bucket-results231'
+            s3 = boto3.client('s3', region_name='us-east-1')
+            s3.put_object(
+                Bucket=output_bucket_name,
+                Key=message_body['id'],
+                Body=result
+            )
+            logging.info("Image sent to Output Bucket successfully.")
+            # Send classified image results to SQS Response Queue
+            message_response = {
+                'id':message_body['id'],
+                'results':result
+            }
+            response = sqs.send_message(
+                QueueUrl= response_queue_url,
+                MessageBody= json.dumps(message_response)
+            )
+
+            # Delete the message from the queue
+            logging.info("Deleting message from request queue")
+            receipt_handle = message['ReceiptHandle']
+            sqs.delete_message(
+                QueueUrl=queue_url,
+                ReceiptHandle=receipt_handle
+            )
+            # Introduce a delay
+            logging.info("Sleeping for 5 sec")
+            time.sleep(5)
+            logging.info("Awake!")
+        else:
+            logging.info("No messages in queue")
+except Exception as exp:
+    logging.error(exp)
 
